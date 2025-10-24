7689f44d29f45e2ba72397ddf43d607d710786ba
Bhuvanesh66 <bhuvaneshms60@gamil.com>
2025-10-24 22:05:51 +0530

    fixed the bug

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
   darkMode: "class",
+  safelist: [
+    'text-gray-400',
+    'dark:text-gray-500'
+  ],
   theme: {
     extend: {
       colors: {
